855931844c89528624b10286f6fbbbd359aa7dfd
Witoslaw Koczewski <wi@koczewski.de>
2020-11-15 21:30:34 +0100

    book sections

--- src/spa/radar.cljs
+++ src/spa/radar.cljs
@@ -41,24 +41,61 @@
              (-> book :title))))))
 
 
+(defnc Section [{:keys [section books]}]
+  ($ ui/Stack
+   ($ mui/Typography
+      {:component "h3"
+       :variant "h5"}
+      (-> section :name))
+   (for [book (->> books (sort-by book/book-recommendation-count) reverse)]
+     ($ Book
+        {:key (api/doc-id book)
+         :book book}))))
+
+
+(def sections
+  [{:key :must-read
+    :name "Must Read"
+    :idx 0}
+   {:key :should-read
+    :name "Should Read"
+    :idx 1}
+   {:key :trial
+    :name "Trial"
+    :idx 2}
+   {:key :to-read
+    :name "To Read"
+    :idx 3}
+   ])
+
+
 (defnc Radar []
   (let [radar (use-radar)
-        books (use-books)]
+        books (use-books)
+        books-by-section-key (->> books
+                                  (group-by #(let [c (-> % :recommendations count)]
+                                               (cond
+                                                 (>= c 5) :must-read
+                                                 (>= c 2) :should-read
+                                                 (>= c 1) :trial
+                                                 :else :to-read))))]
     ($ ui/Stack
-     ($ mui/Typography
-        {:variant "h4"
-         :component "h2"}
-        (-> radar :name))
-     ($ mui/Button
-        {:onClick #(book/show-book-form (api/doc-id radar) nil)
-         :variant "contained"
-         :color "secondary"}
-        "New Book")
-     ($ ui/Stack
-        (for [book (->> books (sort-by book/book-recommendation-count) reverse)]
-          ($ Book
-             {:key (api/doc-id book)
-              :book book}))))))
+       ($ mui/Typography
+          {:variant "h4"
+           :component "h2"}
+          (-> radar :name))
+       ($ mui/Button
+          {:onClick #(book/show-book-form (api/doc-id radar) nil)
+           :variant "contained"
+           :color "secondary"}
+          "New Book")
+       ($ ui/Stack
+          (for [section sections]
+            ($ Section
+               {:key (-> section :idx)
+                :section section
+                :books (get books-by-section-key (-> section :key))}))
+          ))))
 
 
 (defnc PageContent []
