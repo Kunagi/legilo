a2d432ec369059fbb267a84d255f15215a1e365d
Witoslaw Koczewski <wi@koczewski.de>
2020-11-17 22:00:56 +0100

    amazon

--- src/spa/amazon.cljs
+++ src/spa/amazon.cljs
@@ -34,7 +34,7 @@
 (defnc SearchWidget[{:keys [title]}]
   (div
    {:id "AMAZSEARCH"}
-   ($ :script "
+   ($ :script (str "
 amzn_assoc_ad_type ='responsive_search_widget';
 amzn_assoc_tracking_id ='" partner-id "';
 amzn_assoc_marketplace ='amazon';
@@ -46,6 +46,6 @@ amzn_assoc_height ='250';
 amzn_assoc_default_search_category ='';
 amzn_assoc_default_search_key ='" title "';
 amzn_assoc_theme ='light';
-amzn_assoc_bg_color ='FFFFFF';")
+amzn_assoc_bg_color ='FFFFFF';"))
    ($ :script
-      {:src "//z-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=DE"})))
+      {:src "https://z-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=DE"})))
