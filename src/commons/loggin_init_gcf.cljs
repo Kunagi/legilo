4853a2a7770f05b2708252570fb7606fed01caa1
Witoslaw Koczewski <wi@koczewski.de>
2021-02-01 12:27:17 +0100

    moved commons to spark project

--- src/commons/loggin_init_gcf.cljs
+++
@@ -1,7 +0,0 @@
-(ns commons.loggin-init-gcf
-  (:require
-   ["firebase-functions" :as functions]
-   [commons.logging :as logging]))
-
-
-(reset! logging/WRITER (partial logging/console-writer (-> functions .-logger)))
