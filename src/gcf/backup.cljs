c85b05ffdb2d6e1f9311b6ef107e04057f782f94
Witoslaw Koczewski <wi@koczewski.de>
2021-04-26 00:00:30 +0200

    backups

--- src/gcf/backup.cljs
+++ src/gcf/backup.cljs
@@ -5,40 +5,52 @@
    [spark.utils :as u]
    [spark.gcf :as gcf]))
 
-(js/console.log "hello")
 
+(defn bucket []
+  (-> ^js admin .storage (.bucket "gs://legilo-backups")))
+
+(defn file [path]
+  (let [bucket (bucket)
+        file   (-> ^js bucket (.file path))]
+    file))
 
 (comment
   (u/=> (firestore/col> [ "radars"])
         (fn [result]
           (js/console.log "DEBUG-1" result))))
 
-(defn write-string [path value]
-  (js/console.log "write-string" path)
-  (let [bucket   (-> admin .storage (.bucket "gs://legilo-backups"))
-        file     (-> bucket (.file path))
-        writable (-> file .createWriteStream)]
-    (-> writable (.write value))
-    (-> writable .end)
-    path))
+(defn write-string> [path value]
+  (u/promise>
+   (fn [resolve _reject]
+     (let [f        (file path)
+           writable (-> ^js f .createWriteStream)]
+       (-> writable ^js (.on "finish" #(resolve path)))
+       (-> writable ^js (.write value))
+       (-> writable ^js .end)
+       ))))
+
+(defn download-string [path]
+  (let [file (file path)]
+    (-> ^js file .download)))
 
 (comment
-  (sequential? [])
-  (write-string "test.txt" "test"))
+  (u/=> (write-string> "test.txt" "test")
+        u/tap>)
+  (u/=> (download-string "test.txt")
+        (fn [^js buffer]
+          (-> buffer .toString))
+        u/tap>))
 
 
 
-(defn write-doc [path doc]
+(defn write-doc> [path doc]
   (let [path (str path "/" (-> doc :firestore/path) ".edn")
         s    (u/->edn doc)]
-    (write-string path s)))
+    (write-string> path s)))
 
 (defn backup-col> [path col-name]
   (u/=> (firestore/col> [col-name])
-        (fn [docs]
-          (->> docs
-               (map #(write-doc path %))
-               doall))))
+        #(u/all> (map (partial write-doc> path) %))))
 
 (defn backup-all> []
   (let [path (str (if goog.DEBUG "dev" "prod")
@@ -49,8 +61,7 @@
           (map #(backup-col> path %))))))
 
 (defn handle-on-backup> [^js _req]
-  (u/=> (backup-all>)
-        clj->js))
+  (backup-all>))
 
 (comment
   (backup-all>)
