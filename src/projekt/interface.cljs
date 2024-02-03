18f6decddcc9d18528d47d68f208460e6b13e743
Witoslaw Koczewski <wi@koczewski.de>
2024-02-03 11:34:32 +0100

    [cleanup]

--- src/projekt/interface.cljs
+++
@@ -1,9 +0,0 @@
-(ns projekt.interface
-  (:require
-   [projekt.storymap-page :as storymap-page]
-   [projekt.issues-page :as issues-page]))
-
-
-(def storymap-page storymap-page/storymap-page)
-
-(def issues-page issues-page/issues-page)
